d00d09a1ccff78fb3355a8fc63d88970dfa61534
Shir Goldberg <me@shirgoldbird.com>
2021-04-27 16:56:08 -0400

    fix accessibility on music page

--- frontend/src/pages/music.js
+++ frontend/src/pages/music.js
@@ -1,11 +1,8 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import { FaBandcamp, FaSpotify } from 'react-icons/fa'
-import SEO from '../components/SEO'
+import Seo from '../components/Seo'
 
-const MusicStyles = styled.div`
-  justify-content: center;
-`
 const BandcampButton = styled.a`
   background-color: rgb(20,157,203);
   margin-top: 1em;
@@ -18,27 +15,27 @@ const SpotifyButton = styled.a`
 export default function MusicPage() {
   return (
     <>
-      <SEO title={"Listen"} />
-    <h1>Listen</h1>
-    <h2>Let your ears nosh on some auditory treats.</h2>
-    <BandcampButton href="https://glitchkibbitz.bandcamp.com/releases" target="_blank" className="button"><FaBandcamp className="icon" />Bandcamp</BandcampButton>
-    <SpotifyButton href="https://open.spotify.com/artist/3YGg09DqnM5l5pdF6q07Qf?si=SnPj4cd5SOCt9xWjWlfKWw" target="_blank" className="button"><FaSpotify className="icon" /> Spotify</SpotifyButton>
-    <div class="bandcamp-album">
-        <iframe style={{ border: '0', height: '150px' }} src="https://bandcamp.com/EmbeddedPlayer/album=1366043256/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://glitchkibbitz.bandcamp.com/album/misirlou-sisters">Misirlou Sisters by Glitch Kibbitz</a></iframe>
-    </div>
+      <Seo title={"Music"} />
+      <h1>Music</h1>
+      <h2>Let your ears nosh on some auditory treats.</h2>
+      <BandcampButton href="https://glitchkibbitz.bandcamp.com/releases" target="_blank" className="button"><FaBandcamp className="icon" /> Bandcamp</BandcampButton>
+      <SpotifyButton href="https://open.spotify.com/artist/3YGg09DqnM5l5pdF6q07Qf?si=SnPj4cd5SOCt9xWjWlfKWw" target="_blank" className="button"><FaSpotify className="icon" /> Spotify</SpotifyButton>
+      <div className="bandcamp-album">
+          <iframe title="Misirlou Sisters by Glitch Kibbitz" style={{ border: '0', height: '150px' }} src="https://bandcamp.com/EmbeddedPlayer/album=1366043256/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://glitchkibbitz.bandcamp.com/album/misirlou-sisters">Misirlou Sisters by Glitch Kibbitz</a></iframe>
+      </div>
 
-    <div class="bandcamp-album">
-      <iframe style={{border: '0', height: '307px'}} src="https://bandcamp.com/EmbeddedPlayer/album=153649330/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/" seamless><a href="https://glitchkibbitz.bandcamp.com/album/glitch-kibbitz">Glitch Kibbitz by Glitch Kibbitz</a></iframe>
-    </div>
+      <div className="bandcamp-album">
+        <iframe title="Glitch Kibbitz by Glitch Kibbitz" style={{border: '0', height: '307px'}} src="https://bandcamp.com/EmbeddedPlayer/album=153649330/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/" seamless><a href="https://glitchkibbitz.bandcamp.com/album/glitch-kibbitz">Glitch Kibbitz by Glitch Kibbitz</a></iframe>
+      </div>
 
-    <h2>Performances</h2>
+      <h2>Performances</h2>
 
-    <div class="video">
-      <iframe width="100%" height="432" src="https://www.youtube.com/embed/3SrmjG50jZU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>
-    <div class="video">
-      <iframe width="100%" height="432" src="https://www.youtube.com/embed/l4vw0j5zLlU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>
+      <div className="video">
+        <iframe title="MAGFest 2020 Performance" width="100%" height="432" src="https://www.youtube.com/embed/3SrmjG50jZU" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>MAGFest 2020 Performance</iframe>
+      </div>
+      <div className="video">
+        <iframe title="PAX West 2019 Performance" width="100%" height="432" src="https://www.youtube.com/embed/l4vw0j5zLlU" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>PAX West 2019 Performance</iframe>
+      </div>
     </>
   );
 }
