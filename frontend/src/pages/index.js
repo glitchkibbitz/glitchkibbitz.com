6e6fe051690116a4444d6e4118c00930832c1673
Shir Goldberg <me@shirgoldbird.com>
2021-04-27 16:56:16 -0400

    improve copy on homepage

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import { FaEnvelope } from 'react-icons/fa'
-import SEO from '../components/SEO'
+import Seo from '../components/Seo'
 
 const Heading = styled.h3`
   font-style: italic;
@@ -11,15 +11,15 @@ const Heading = styled.h3`
 export default function IndexPage() {
   return (
     <>
-      <SEO title={"Home"} />
+      <Seo title={"Home"} />
       <h1>Klezmer + VGM tunes = win</h1>
-      <Heading>Glitch Kibbitz is a Seattle band creating klezmer-inspired renditions of classic video game tunes.</Heading>
+      <Heading>Glitch Kibbitz is the first of its kind—a VGM cover band specializing in klezmerized interpretations of classic video game tunes.</Heading>
       <p>
-        GK has performed at PAX, MAGFest, and basements near you. Since forming in 2018, they've released two albums and one video game. Whether your childhood nostalgia is Megaman or mazel tovs, their music is sure to strike a chord! 
+        Formed in 2019 to perform at PAX West, Glitch Kibbitz has gone on to produce tracks for Materia Collective, perform at MAGFest, and release their own video games. Whether your childhood nostalgia is Megaman or mazel tovs, their music is sure to strike a chord!
       <br />< br/>
         Like what you <a href="/music">hear</a>? Sign up for the email list and always be the first to know when new releases, launch parties, and live shows are coming up.
       </p>
-      <a href="http://eepurl.com/gCr7S5" target="_blank" class="button"><FaEnvelope className="icon" /> Kibbitz with us!</a>
+      <a href="http://eepurl.com/gCr7S5" target="_blank" rel="noopener noreferrer" className="button"><FaEnvelope className="icon" /> Kibbitz with us!</a>
     </>
   );
 }
