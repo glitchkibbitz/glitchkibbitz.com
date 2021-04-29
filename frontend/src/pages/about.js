f6db6f937f3ef49ae32862da7666a850c8eb28f7
Shir Goldberg <me@shirgoldbird.com>
2021-04-28 22:07:54 -0400

    tweak language

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -14,7 +14,7 @@ export default function AboutPage() {
             <p>
                 Shortly before PAX West 2019, a stroke of good mazel brought Glitch Kibbitz into existence. Their first performance took place in PAX's Jamspace, featuring 9 original arrangements of video game tunes—all written and practiced in the month before the event. (Hopefully it didn't show too much...)
             </p><p>
-                After PAX, GK continued to expand their repertoire, and followed up with a performance at the MAGFest 2020 Belvedere Lobby Bar. Over 2020 and 2021, they collaborated remotely, creating several tracks for Materia Collective albums and releasing two singles.
+                After PAX, GK continued to expand their repertoire, and followed up with a performance at the MAGFest 2020 Belvedere Lobby Bar. Over 2020 and 2021, they collaborated remotely, creating several tracks for Materia Community albums, 2 singles, and a self-titled EP.
             </p>
             <p>Glitch Kibbitz is composed of 5 musicians:
             <MusicianList>
